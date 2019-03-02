439a675a70e2decab2e6658bac7d73f4253b0b5a
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-03-02 16:04:22 +0100

    First commit

---
+++ Software/bbusb/frmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace bbusb
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bbusbPort = new System.IO.Ports.SerialPort(this.components);
+            this.lblDeviceStatus = new System.Windows.Forms.Label();
+            this.pbrCalibration = new System.Windows.Forms.ProgressBar();
+            this.pbrLeft = new System.Windows.Forms.ProgressBar();
+            this.pbrRight = new System.Windows.Forms.ProgressBar();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.txtTreshold = new System.Windows.Forms.TextBox();
+            this.lblCenter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDeviceStatus
+            // 
+            this.lblDeviceStatus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDeviceStatus.Font = new System.Drawing.Font("Segoe UI Semilight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeviceStatus.Location = new System.Drawing.Point(0, 0);
+            this.lblDeviceStatus.Name = "lblDeviceStatus";
+            this.lblDeviceStatus.Size = new System.Drawing.Size(331, 41);
+            this.lblDeviceStatus.TabIndex = 2;
+            this.lblDeviceStatus.Text = "Initializing";
+            this.lblDeviceStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbrCalibration
+            // 
+            this.pbrCalibration.BackColor = System.Drawing.SystemColors.Control;
+            this.pbrCalibration.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbrCalibration.Location = new System.Drawing.Point(0, 41);
+            this.pbrCalibration.Name = "pbrCalibration";
+            this.pbrCalibration.Size = new System.Drawing.Size(331, 23);
+            this.pbrCalibration.TabIndex = 3;
+            this.pbrCalibration.Visible = false;
+            // 
+            // pbrLeft
+            // 
+            this.pbrLeft.Location = new System.Drawing.Point(6, 90);
+            this.pbrLeft.Name = "pbrLeft";
+            this.pbrLeft.Size = new System.Drawing.Size(100, 23);
+            this.pbrLeft.TabIndex = 4;
+            // 
+            // pbrRight
+            // 
+            this.pbrRight.Location = new System.Drawing.Point(219, 90);
+            this.pbrRight.Name = "pbrRight";
+            this.pbrRight.Size = new System.Drawing.Size(100, 23);
+            this.pbrRight.TabIndex = 5;
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Location = new System.Drawing.Point(3, 74);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(25, 13);
+            this.lblLeft.TabIndex = 6;
+            this.lblLeft.Text = "Left";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(216, 74);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(32, 13);
+            this.lblRight.TabIndex = 7;
+            this.lblRight.Text = "Right";
+            // 
+            // txtTreshold
+            // 
+            this.txtTreshold.Location = new System.Drawing.Point(113, 90);
+            this.txtTreshold.Name = "txtTreshold";
+            this.txtTreshold.Size = new System.Drawing.Size(100, 20);
+            this.txtTreshold.TabIndex = 8;
+            this.txtTreshold.Text = "20000";
+            // 
+            // lblCenter
+            // 
+            this.lblCenter.AutoSize = true;
+            this.lblCenter.Location = new System.Drawing.Point(110, 74);
+            this.lblCenter.Name = "lblCenter";
+            this.lblCenter.Size = new System.Drawing.Size(38, 13);
+            this.lblCenter.TabIndex = 9;
+            this.lblCenter.Text = "Center";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 121);
+            this.Controls.Add(this.lblCenter);
+            this.Controls.Add(this.txtTreshold);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.pbrRight);
+            this.Controls.Add(this.pbrLeft);
+            this.Controls.Add(this.pbrCalibration);
+            this.Controls.Add(this.lblDeviceStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "bbusb";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort bbusbPort;
+        private System.Windows.Forms.Label lblDeviceStatus;
+        private System.Windows.Forms.ProgressBar pbrCalibration;
+        private System.Windows.Forms.ProgressBar pbrLeft;
+        private System.Windows.Forms.ProgressBar pbrRight;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.TextBox txtTreshold;
+        private System.Windows.Forms.Label lblCenter;
+    }
+}
+
---
+++ Software/bbusb/frmMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bbusb
+{
+    public partial class frmMain : Form
+    {
+        bool isForwardDown = false;
+        bool isBackwardDown = false;
+        int threshold = 20000;
+
+        public enum BBStatus
+        {
+            UNKNOWN,
+            CONNECTED,
+            CALIBRATING,
+            VALUES
+        }
+
+        private string buffer;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            bbusbPort.DataReceived += BbusbPort_DataReceived;
+
+
+            AutoConnectIfPossible(true);
+        }
+
+        private void AutoConnectIfPossible(bool firstTime = false)
+        {
+            if (!bbusbPort.IsOpen || firstTime)
+            {
+                lblDeviceStatus.Text = "Disconnected";
+                lblDeviceStatus.BackColor = Color.LightGray;
+
+                string[] ports = SerialPort.GetPortNames();
+                if (ports.Contains(Properties.Settings.Default.bbusbport))
+                {
+                    bbusbPort.PortName = Properties.Settings.Default.bbusbport;
+                    try
+                    {
+                        bbusbPort.Open();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+
+        delegate void UpdateUIDelegate(BBStatus status, string data);
+        void UpdateUI(BBStatus status, string data)
+        {
+            pbrCalibration.Visible = false;
+
+            switch (status)
+            {
+                case BBStatus.UNKNOWN:
+                    lblDeviceStatus.Text = "Unknown status";
+                    lblDeviceStatus.BackColor = Color.LightGray;
+                    break;
+                case BBStatus.CONNECTED:
+                    lblDeviceStatus.Text = "Connected";
+                    lblDeviceStatus.BackColor = Color.Green;
+                    break;
+                case BBStatus.CALIBRATING:
+                    lblDeviceStatus.Text = "Calibrating";
+                    lblDeviceStatus.BackColor = Color.Yellow;
+                    pbrCalibration.Value = Math.Min(100, data.Length * 2);
+                    pbrCalibration.Visible = true;
+                    break;
+                case BBStatus.VALUES:
+                    lblDeviceStatus.Text = "Receiving";
+                    lblDeviceStatus.BackColor = Color.White;
+
+                    int right, left, center;
+                    string[] values = data.Split(',');
+
+                    if (values.Length >= 3)
+                    {
+                        int.TryParse(values[0], out right);
+                        int.TryParse(values[1], out left);
+                        int.TryParse(values[2], out center);
+
+                        if (left > pbrLeft.Maximum) pbrLeft.Maximum = left;
+                        if (right > pbrRight.Maximum) pbrRight.Maximum = right;
+                        pbrLeft.Value = left;
+                        pbrRight.Value = right;
+
+                        lblLeft.Text = left.ToString();
+                        lblRight.Text = right.ToString();
+                        lblCenter.Text = center.ToString();
+
+                        int.TryParse(txtTreshold.Text, out threshold);
+
+                        if (center > threshold)
+                        {
+                            if (!isForwardDown) InputManager.Keyboard.KeyDown(Keys.S);
+                            isForwardDown = true;
+
+                            if (isBackwardDown) InputManager.Keyboard.KeyUp(Keys.Z);
+                            isBackwardDown = false;
+                        }
+                        else if (center < -threshold)
+                        {
+                            if (isForwardDown) InputManager.Keyboard.KeyUp(Keys.S);
+                            isForwardDown = false;
+
+                            if (!isBackwardDown) InputManager.Keyboard.KeyDown(Keys.Z);
+                            isBackwardDown = true;
+                        }
+                        else
+                        {
+                            if (isForwardDown) InputManager.Keyboard.KeyUp(Keys.S);
+                            isForwardDown = false;
+
+                            if (isBackwardDown) InputManager.Keyboard.KeyUp(Keys.Z);
+                            isBackwardDown = false;
+                        }
+
+                    }
+                    break;
+            }
+        }
+
+        private void BbusbPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            buffer += bbusbPort.ReadExisting();
+
+            if (buffer.StartsWith(".")) BeginInvoke(new UpdateUIDelegate(UpdateUI), BBStatus.CALIBRATING, buffer);
+
+            if (buffer.Contains("\r\n"))
+            {
+                string[] splitBuffer = buffer.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                string value = splitBuffer[0];
+
+                BBStatus status = BBStatus.UNKNOWN;
+                if (value.StartsWith("bbusb")) status = BBStatus.CONNECTED;
+                else if (value.StartsWith(".")) status = BBStatus.CALIBRATING;
+                else if (value.StartsWith("c")) status = BBStatus.CALIBRATING;
+                else if (value.Contains(",")) status = BBStatus.VALUES;
+                else status = BBStatus.UNKNOWN;
+
+                BeginInvoke(new UpdateUIDelegate(UpdateUI), status, value);
+
+                if (splitBuffer.Length > 1) buffer = splitBuffer[1];
+                else buffer = "";
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            //WM_DEVICECHANGE
+            if (m.Msg == 0x219)
+            {
+                AutoConnectIfPossible();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bbusbPort.Close();
+        }
+    }
+}
